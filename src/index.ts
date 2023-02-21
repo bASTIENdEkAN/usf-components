6b5b4e1473959ca252ebf20b75099a30290e7f39
Bastien De Kan <bastien.dekan@springbokagency.com>
2023-02-21 13:52:06 +0100

    fix: export component

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export * from './components';
+// https://github.com/ionic-team/stencil/issues/2826
+// export * from './components';
+export { Modal } from './components/modal/modal';
+export { StickyHeader } from './components/sticky-header/sticky-header';
